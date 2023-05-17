17916319d75c213c4036fb05db029900750c7dd3
mohamadghoroobi <m.ghoroobi@gmail.com>
2023-05-17 10:17:54 +0330

    edit contact.py
    class Supplier is added.

--- Chapter03/contact.py
+++ Chapter03/contact.py
@@ -5,3 +5,11 @@ class Contact:
         self.name = name
         self.email = email
         Contact.all_contacts.append(self)
+
+
+class Supplier(Contact):
+    def order(self, order):
+        print(
+            "If this were a real system we would send "
+            f"'{order}' order to '{self.name}'"
+        )
