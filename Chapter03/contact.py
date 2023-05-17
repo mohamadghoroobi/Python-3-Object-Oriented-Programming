57762811d09b0957da433ab41b23287531ed9d0f
mohamadghoroobi <m.ghoroobi@gmail.com>
2023-05-17 10:35:33 +0330

    edit contact.py
    class ContactList added.
    func search added.

--- Chapter03/contact.py
+++ Chapter03/contact.py
@@ -1,3 +1,13 @@
+class ContactList(list):
+    def search(self, name):
+        """Return all contacts that contain the search value
+        in their name."""
+        matching_contacts = []
+        for contact in self:
+            if name in contact.name:
+                matching_contacts.append(contact)
+        return matching_contacts
+
 class Contact:
     all_contacts = []
 
