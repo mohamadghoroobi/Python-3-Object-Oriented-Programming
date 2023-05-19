f27e7219ee2cef076f847162990ebe10723c6854
Mohammad Ghoroobi Lenove <m.ghoroobi@gmail.com>
2023-05-19 23:01:06 +0330

    edit audio_player.py
    add class MP3File inherit AudioFile
    add func play

--- Chapter03/audio_player.py
+++ Chapter03/audio_player.py
@@ -3,3 +3,10 @@ class AudioFile:
         if not filename.endswith(self.ext):
             raise Exception("Invalid file format")
         self.filename = filename
+
+
+class MP3File(AudioFile):
+    ext = "mp3"
+
+    def play(self):
+        print("playing {} as mp3".format(self.filename))
