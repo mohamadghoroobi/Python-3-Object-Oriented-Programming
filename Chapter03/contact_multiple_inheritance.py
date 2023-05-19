626ea8659f83e2950c237b3786c6f710e6c230d6
Mohammad Ghoroobi Lenove <m.ghoroobi@gmail.com>
2023-05-19 15:38:48 +0330

    edit contact_multiple_inheritance.py
    create class Friend

--- Chapter03/contact_multiple_inheritance.py
+++ Chapter03/contact_multiple_inheritance.py
@@ -13,3 +13,11 @@ class AddressHolder:
         self.city = city
         self.state = state
         self.code = code
+
+class Friend(Contact, AddressHolder):
+    def __int__(
+            self, name, email, phone, street, city, state, code):
+        Contact.__init__(self, name, email)
+        AddressHolder.__init__(self, street, city, state, code)
+        self.phone = phone
+
