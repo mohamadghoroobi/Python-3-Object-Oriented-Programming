3909ef95b57de8a7a2588a9dabdc33d980f0f79e
Mohammad Ghoroobi Lenove <m.ghoroobi@gmail.com>
2023-05-19 15:51:01 +0330

    edit multiple_inheritance_bad_demo.py
    create class LeftSubclass inherit BaseClass
    create func call_me

--- Chapter03/multiple_inheritance_bad_demo.py
+++ Chapter03/multiple_inheritance_bad_demo.py
@@ -3,4 +3,13 @@ class BaseClass:
 
     def call_me(self):
         print("Calling method on Base Class")
-        self.num_base_class
+        self.num_base_class += 1
+
+
+class LeftSubclass(BaseClass):
+    num_left_class = 0
+
+    def call_me(self):
+        BaseClass.call_me(self)
+        print("Calling method on Left Subclass")
+        self.num_left_class += 1
