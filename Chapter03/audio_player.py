42a6709c7acee282de42bfeb6574278b1491664a
Mohammad Ghoroobi Lenove <m.ghoroobi@gmail.com>
2023-05-19 23:45:17 +0330

    edit audio_player.py
    fix func play

--- Chapter03/audio_player.py
+++ Chapter03/audio_player.py
@@ -1,7 +1,8 @@
 class AudioFile:
-    def __int__(self, filename):
+    def __init__(self, filename):
         if not filename.endswith(self.ext):
             raise Exception("Invalid file format")
+
         self.filename = filename
 
 
@@ -17,6 +18,7 @@ class WavFile(AudioFile):
 
     def play(self):
         print("playing {} as wav".format(self.filename))
+
 
 class OggFile(AudioFile):
     ext = "ogg"
