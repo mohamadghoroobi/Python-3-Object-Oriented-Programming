e680698cf8f120f88035c8bda66ec7b1a868f437
mohamadghoroobi <m.ghoroobi@gmail.com>
2023-05-20 13:04:48 +0330

    edit file lessons.py
    add class IntroToPython
    
    add func lesson

---
+++ Chapter03/Case_Study_Online_Grader/lessons.py
@@ -0,0 +1,7 @@
+class IntroToPython:
+    def lesson(self):
+        return f"""
+        Hello {self.student}. define two variables,
+        an integer named a with value 1
+        and a string named b with value 'hello'
+        """
