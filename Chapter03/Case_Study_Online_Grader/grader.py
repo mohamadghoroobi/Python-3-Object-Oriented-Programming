c0bbc7ff7938abc7461f93d5f6d22035d7a7526b
mohamadghoroobi <m.ghoroobi@gmail.com>
2023-05-20 13:11:02 +0330

    edit file grader.py
    edit class Assignments
    
    add func lesson, check, __subclasshook__

--- Chapter03/Case_Study_Online_Grader/grader.py
+++ Chapter03/Case_Study_Online_Grader/grader.py
@@ -2,4 +2,19 @@ import abc
 
 
 class Assignments(metaclass=abc.ABCMeta):
-    pass
+    @abc.abstractmethod
+    def lesson(self, student):
+        pass
+
+    @abc.abstractmethod
+    def check(self, code):
+        pass
+
+    @classmethod
+    def __subclasshook__(cls, C):
+        if cls in Assignments:
+            attrs = set(dir(C))
+            if set(cls.__abstactmethods__) <= attrs:
+                return True
+
+        return NotImplemented
