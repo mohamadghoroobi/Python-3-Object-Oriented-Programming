f49d80591f4b983b12fd902593845c9f3c872509
mohamadghoroobi <m.ghoroobi@gmail.com>
2023-05-20 13:05:40 +0330

    edit file lessons.py
    edit class IntroToPython
    
    add func check

--- Chapter03/Case_Study_Online_Grader/lessons.py
+++ Chapter03/Case_Study_Online_Grader/lessons.py
@@ -5,3 +5,6 @@ class IntroToPython:
         an integer named a with value 1
         and a string named b with value 'hello'
         """
+
+    def check(self, code):
+        return code == "a = 1\nb = 'hello'"
