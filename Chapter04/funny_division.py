2e5bccbe94f68dbf246567305aca685394c3cc0d
mohamadghoroobi <m.ghoroobi@gmail.com>
2023-05-21 13:36:10 +0330

    add file funny_division.py
    add func funny_division3

--- Chapter04/funny_division.py
+++ Chapter04/funny_division.py
@@ -14,10 +14,29 @@ def funny_division2(divider):
     try:
         if divider == 13:
             raise ValueError("13 is unlocky number")
-        return 100/divider
+        return 100 / divider
     except(ZeroDivisionError, TypeError):
         return "Enter a number another than zero."
+
 
 for val in (0, "hello", 50.0, 13):
     print("Testing {}:".format(val), end=" ")
     print(funny_division2(val))
+
+
+def funny_division3(divider):
+    try:
+        if divider == 13:
+            raise ValueError("13 is an unlucky number.")
+        return 100 / divider
+    except ZeroDivisionError:
+        return 'Enter a number other than zero'
+    except TypeError:
+        return "Enter a numeical value"
+    except ValueError:
+        print("No, No, not 13.")
+        raise
+
+for val in (0, "hello", 50.0, 13):
+    print("Testing {}:".format(val), end=" ")
+    print(funny_division3(val))
