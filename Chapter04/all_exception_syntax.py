00f57a7965067aeb466caa06bda150684f52a303
mohamadghoroobi <m.ghoroobi@gmail.com>
2023-05-21 13:51:50 +0330

    edit file all_exception_syntax.py
    test code

--- Chapter04/all_exception_syntax.py
+++ Chapter04/all_exception_syntax.py
@@ -1,4 +1,25 @@
+# try:
+#     raise ValueError("This is an argument.")
+# except ValueError as e:
+#     print("The exception arguments were", e.args)
+
+
+import random
+some_exceptions = [ValueError, TypeError, IndexError, None]
 try:
-    raise ValueError("This is an argument.")
-except ValueError as e:
-    print("The exception arguments were", e.args)
+    choice = random.choice(some_exceptions)
+    print("raising {}".format(choice))
+    if choice:
+        raise choice("An error")
+except ValueError:
+    print("Caught a ValueError")
+except TypeError:
+    print("Caught a TypeError")
+except Exception as e:
+    print("Caught some other errors: %s" %
+          (e.__class__.__name__))
+else:
+    print("This code called if there is no exception")
+finally:
+    print("This cleanup code is always called.")
+
