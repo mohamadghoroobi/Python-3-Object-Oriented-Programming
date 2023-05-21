162debec245192e627e1a150d938752cf2a43b39
mohamadghoroobi <m.ghoroobi@gmail.com>
2023-05-21 12:26:59 +0330

    edit file no_return.py
    add func call_exceptor

--- Chapter04/no_return.py
+++ Chapter04/no_return.py
@@ -3,3 +3,10 @@ def no_return():
     raise Exception("This is always raised")
     print("This line will never execute")
     return "I won't be returned."
+
+
+def call_exceptor():
+    print("call_exceptor starts here...")
+    no_return()
+    print("an exception was raised")
+    print("...so theses lines don't run")
