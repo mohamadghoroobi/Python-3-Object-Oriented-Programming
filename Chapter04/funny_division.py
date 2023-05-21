4e7e5217d2740ead6b31972d19e4126d7ecdf68c
mohamadghoroobi <m.ghoroobi@gmail.com>
2023-05-21 13:29:53 +0330

    add file funny_division.py
    add func funny_division2

--- Chapter04/funny_division.py
+++ Chapter04/funny_division.py
@@ -8,3 +8,16 @@ def funny_division(divider):
 print(funny_division(0))
 print(funny_division(50.0))
 print(funny_division("hello"))
+
+
+def funny_division2(divider):
+    try:
+        if divider == 13:
+            raise ValueError("13 is unlocky number")
+        return 100/divider
+    except(ZeroDivisionError, TypeError):
+        return "Enter a number another than zero."
+
+for val in (0, "hello", 50.0, 13):
+    print("Testing {}:".format(val), end=" ")
+    print(funny_division2(val))
