707cb9e1a1f49c96b40af41dacac9946e0bd86cd
mohamadghoroobi <m.ghoroobi@gmail.com>
2023-05-24 10:46:58 +0330

    edit file invetory.py
    add class OutOfStock

--- Chapter04/invetory.py
+++ Chapter04/invetory.py
@@ -17,4 +17,36 @@ class InvalidWithdrawl(Exception):
 
 
 class Inventory:
+    def lock(self, item_type):
+        """Select the type of item that is going to
+        be manipulated. This method will lock the
+        item so nobody else can manipulate the
+        inventory until it's returned. This prevents
+        selling the same item to two different
+        customers."""
+        pass
+
+    def unlock(self, item_type):
+        """Release the given type so that other
+        customers can access it."""
+        pass
+
+    def purchase(self, item_type):
+        """If the item is not locked, raise an
+        exception. If the item_type does not exist,
+        raise an exception. If the item is currently
+        out of stock, raise an exception. If the item
+        is available, subtract one item and return
+        the number of items left."""
+        pass
+
+
+class OutOfStock(Exception):
     pass
+
+# item_type = "widget"
+# inv = Inventory()
+# inv.lock(item_type)
+# try:
+#     num_left = inv.purchase(item_type)
+# except Invad
