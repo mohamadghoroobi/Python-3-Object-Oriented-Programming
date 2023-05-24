ec77f0d24a8c3eb6f76e79e30603251a0b50f0da
mohamadghoroobi <m.ghoroobi@gmail.com>
2023-05-24 14:15:29 +0330

    edit file auth.py
    add class AuthException

--- Chapter04/Case Study_ Authorization/auth.py
+++ Chapter04/Case Study_ Authorization/auth.py
@@ -1,5 +1,10 @@
 import hashlib
 
+class AuthException(Exception):
+    def __init__(self, username, user=None):
+        super().__init__(username, user)
+        self.username= username
+        self.user = user
 class User:
     def __init__(self, username, password):
         """Create a new user object. The password
