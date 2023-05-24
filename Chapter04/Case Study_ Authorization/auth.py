bc565d524b9c4e14a9b47b1c30bb14f97b1b3050
mohamadghoroobi <m.ghoroobi@gmail.com>
2023-05-24 14:48:48 +0330

    edit file auth.py
    edit class Authenticator
    
    add func login

--- Chapter04/Case Study_ Authorization/auth.py
+++ Chapter04/Case Study_ Authorization/auth.py
@@ -23,6 +23,7 @@ class InvalidUsername(AuthException):
 class InvalidPassword(AuthException):
     pass
 
+
 class User:
     def __init__(self, username, password):
         """Create a new user object. The password
@@ -44,6 +45,7 @@ class User:
         encrypted = self._encrypt_pw(password)
         return encrypted == self.password
 
+
 class Authenticator:
     def __init__(self):
         """Construct an authenticator to manage
@@ -57,9 +59,19 @@ class Authenticator:
             raise PasswordTooShort(username)
         self.users[username] = User(username, password)
 
+    def login(self, username, password):
+        try:
+            user = self.users[username]
+        except KeyError:
+            raise InvalidUsername(username)
 
-    # def login(self, username, password):
-    #     try:
-    #         user = self.users[username]
-    #     except KeyError:
-    #         raise InvalidUsername(username)
+        if not user.check_password(password):
+            raise InvalidPassword(username, user)
+
+        user.is_logged_in = True
+        return True
+
+    def is_logged_in(self, username):
+        if username in self.users:
+            return self.users[username].is_logged_in
+        return False
