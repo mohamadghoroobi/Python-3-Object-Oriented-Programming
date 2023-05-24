dbe5d20fe4e0c3c1474c54cddb4d45f5f405180d
mohamadghoroobi <m.ghoroobi@gmail.com>
2023-05-24 13:58:04 +0330

    edit file auth.py
    edit class User
    
    add func _encrypt_pw

--- Chapter04/Case Study_ Authorization/auth.py
+++ Chapter04/Case Study_ Authorization/auth.py
@@ -7,3 +7,9 @@ class User:
         self.username = username
         # self.passord =
 
+    def _encrypt_pw(self, password):
+        """Encrypt the password with the username and return
+        the sha digest."""
+        has_string = self.username + password
+        hash_string = has_string.encode("utf8")
+        return hashlib.sha256(hash_string).hexdigest()
