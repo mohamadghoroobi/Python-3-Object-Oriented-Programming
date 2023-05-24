6fff0f2cc7db68e4ca0faf3214ee28360adf88c2
mohamadghoroobi <m.ghoroobi@gmail.com>
2023-05-24 13:54:06 +0330

    edit file auth.py
    add class User
    
    add func init

--- Chapter04/Case Study_ Authorization/auth.py
+++ Chapter04/Case Study_ Authorization/auth.py
@@ -1,4 +1,9 @@
 import hashlib
 
 class User:
-    pass
+    def __init__(self, username, password):
+        """Create a new user object. The password
+        will be encrypted before storing."""
+        self.username = username
+        # self.passord =
+
