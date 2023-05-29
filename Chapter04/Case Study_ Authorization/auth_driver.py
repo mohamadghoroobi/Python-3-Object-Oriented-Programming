755f65e29e9b7042ae7e5725a3db5734024e0736
mohamadghoroobi <m.ghoroobi@gmail.com>
2023-05-29 14:26:08 +0330

    edit file auth_driver.py
    add class Editor

---
+++ Chapter04/Case Study_ Authorization/auth_driver.py
@@ -0,0 +1,13 @@
+import auth
+
+#Set up a test user and permission
+auth.authenticator.add_user("joe", "joepassword")
+auth.authorizor.add_permission("test program")
+auth.authorizor.add_permission("change program")
+auth.authorizor.permit_user("test program", "joe")
+
+
+class Editor:
+    # def __init__(self):
+    #     self.username = None
+    #     self.munu_ma
