87411940e933a7fd2ecb112ffa19690fb152cbe7
mohamadghoroobi <m.ghoroobi@gmail.com>
2023-05-29 14:28:23 +0330

    edit file auth_driver.py
    edit class Editor
    
    add func __init

--- Chapter04/Case Study_ Authorization/auth_driver.py
+++ Chapter04/Case Study_ Authorization/auth_driver.py
@@ -1,6 +1,6 @@
 import auth
 
-#Set up a test user and permission
+# Set up a test user and permission
 auth.authenticator.add_user("joe", "joepassword")
 auth.authorizor.add_permission("test program")
 auth.authorizor.add_permission("change program")
@@ -8,6 +8,13 @@ auth.authorizor.permit_user("test program", "joe")
 
 
 class Editor:
-    # def __init__(self):
-    #     self.username = None
-    #     self.munu_ma
+    def __init__(self):
+        self.username = None
+        self.munu_map = {
+            "login": self.login,
+            "test": self.test,
+            "change": self.change,
+            "quit": self.quit,
+        }
+
+
