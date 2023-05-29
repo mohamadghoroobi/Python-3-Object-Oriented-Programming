1ef20896faa0af3dbe0d1a7cebc612d414df33be
mohamadghoroobi <m.ghoroobi@gmail.com>
2023-05-29 15:30:05 +0330

    edit file points.py
    add class Point
    
    add func init, distance

--- Chapter05/points.py
+++ Chapter05/points.py
@@ -7,7 +7,23 @@ def distance(p1, p2):
 
 def perimeter(polygon):
     perimeter = 0
+    # print([polygon[0]])
+    # print(polygon)
     points = polygon + [polygon[0]]
+    # print(points)
     for i in range(len(polygon)):
         perimeter += distance(points[i], points[i + 1])
     return perimeter
+
+
+square = [(1, 1), (1, 2), (2, 2), (2, 1)]
+perimeter(square)
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def distance(self, p2):
+        return math.sqrt((self.x - p2.x) ** 2 + (self.y - p2.y) ** 2)
