e65d9671f922d916ca0def15d9d1b47780058b8c
mohamadghoroobi <m.ghoroobi@gmail.com>
2023-05-30 10:33:28 +0330

    edit file silly.py
    add class SillyDecorated
    
    add func silly (property, setter, deleter)

--- Chapter05/silly.py
+++ Chapter05/silly.py
@@ -12,3 +12,21 @@ class Silly:
         del self._silly
 
     silly = property(_get_silly, _set_silly, _del_silly, "This is silly property.")
+
+
+class SillyDecorated:
+    @property
+    def silly(self):
+        "This is silly property"
+        print("You are getting silly")
+        return self._silly
+
+    @silly.setter
+    def silly(self, value):
+        print("You are making silly {}".format(value))
+        self._silly = value
+
+    @silly.deleter
+    def silly(self):
+        print("Whoah, you killed silly!")
+        del self._silly
