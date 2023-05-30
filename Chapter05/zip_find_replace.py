e55c5884a48de047d00def11109809ee16e2ea0c
mohamadghoroobi <m.ghoroobi@gmail.com>
2023-05-30 11:26:42 +0330

    edit file zip_find_replace.py
    edit class ZipReplace
    
    add func zip_find_replace, unzip_files

--- Chapter05/zip_find_replace.py
+++ Chapter05/zip_find_replace.py
@@ -3,9 +3,24 @@ import shutil
 import zipfile
 from pathlib import Path
 
+
 class ZipReplace:
     def __init__(self, filename, search_string, replace_string):
         self.filename = filename
         self.search_string = search_string
         self.replace_string = replace_string
         self.temp_directory = Path(f"unzipped-{filename}")
+
+    #overall manager
+    def zip_find_replace(self):
+        self.unzip_files()
+        self.find_replace()
+        self.zip_files()
+
+    def unzip_files(self):
+        self.temp_directory.mkdir()
+        with zipfile.ZipFile(self.filename) as zip:
+            zip.extractall(self.temp_directory)
+
+
+
