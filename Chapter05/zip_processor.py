a0b69c544d087f7d77ae52feea6ad5bdaaa36cd1
mohamadghoroobi <m.ghoroobi@gmail.com>
2023-05-31 14:45:15 +0330

    edit file zip_processor.py
    add class ZipReplace

--- Chapter05/zip_processor.py
+++ Chapter05/zip_processor.py
@@ -21,17 +21,25 @@ class ZipProcessor:
         with zipfile.ZipFile(self.zipname) as zip:
             zip.extractall(self.temp_directory)
 
-    # def find_replace(self):
-    #     for filename in self.temp_directory.iterdir():
-    #         with filename.open() as file:
-    #             contents = file.read()
-    #         contents = contents.replace(self.search_string, self.replace_string)
-    #         with filename.open("w") as file:
-    #             file.write(contents)
-
     def zip_files(self):
         with zipfile.ZipFile(self.zipname, "w") as file:
             for filename in self.temp_directory.iterdir():
                 file.write(filename, filename.name)
         shutil.rmtree(self.temp_directory)
 
+
+class ZipReplace:
+    def __init__(self, filename, search_string, replace_string):
+        super.__init__(filename)
+        self.search_string = search_string
+        self.replace_string = replace_string
+
+    def process_files(self):
+        """perform a search and replace on all files in the
+        temporary directory"""
+        for filename in self.temp_directory.iterdir():
+            with filename.open() as file:
+                contents = file.read()
+            contents = contents.replace(self.search_string, self.replace_string)
+            with filename.open("w") as file:
+                file.write(contents)
