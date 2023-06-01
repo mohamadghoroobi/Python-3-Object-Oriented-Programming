ff174209ebc3dab8b38ee5c453837f4acd1cd6df
Mohammad Ghoroobi Lenove <m.ghoroobi@gmail.com>
2023-06-01 17:19:41 +0330

    Chapter05 is finished.

--- Chapter05/Document.py
+++ Chapter05/Document.py
@@ -5,8 +5,10 @@ class Document:
         self.filename = ""
 
     def insert(self, character):
+        if not hasattr(character, "character"):
+            character = Character(character)
         self.characters.insert(self.cursor.position, character)
-        self.cursor += 1
+        self.cursor.forward()
 
     def delete(self):
         del self.characters[self.cursor.position]
@@ -14,6 +16,10 @@ class Document:
     def save(self):
         with open(self.filename, "w") as f:
             f.write("".join(self.characters))
+
+    @property
+    def string(self):
+        return "".join((str(c) for c in self.characters))
 
 
 class Cursor:
@@ -35,8 +41,23 @@ class Cursor:
                 break
 
     def end(self):
-        while(
+        while (
             self.position < len(self.document.characters)
-            and self.document.characters[self.position] != "\n"
+            and self.document.characters[self.position].character != "\n"
         ):
             self.position += 1
+
+
+class Character:
+    def __init__(self, character, bold=False, italic=False, underline=False):
+        assert len(character) == 1
+        self.character = character
+        self.bold = bold
+        self.italic = italic
+        self.underline = underline
+
+    def __str__(self):
+        bold = "*" if self.bold else ""
+        italic = "/" if self.italic else ""
+        underline = "_" if self.underline else ""
+        return bold + italic + underline + self.character
