3fa448b7a61464d0ec6d788dc082adea668889c3
Mohammad Ghoroobi Lenove <m.ghoroobi@gmail.com>
2023-06-01 16:08:45 +0330

    edit file Document.py
    edit class Document
    
    add func insert, delete, save

--- Chapter05/Document.py
+++ Chapter05/Document.py
@@ -3,3 +3,16 @@ class Document:
         self.characters = []
         self.cursor = 0
         self.filename = ""
+
+    def insert(self, character):
+        self.characters.insert(self.cursor, character)
+        self.cursor += 1
+
+    def delete(self):
+        del self.characters[self.cursor]
+
+    def save(self):
+        with open(self.filename, "w") as f:
+            f.write("".join(self.characters))
+
+
