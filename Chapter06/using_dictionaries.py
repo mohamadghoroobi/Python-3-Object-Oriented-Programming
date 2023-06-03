ec2063bd0292f9a4425dcc3b6718413b8f1ef33c
mohamadghoroobi <m.ghoroobi@gmail.com>
2023-06-03 11:32:14 +0330

    edit file using_dictionaries.py
    add dic random_keys

--- Chapter06/using_dictionaries.py
+++ Chapter06/using_dictionaries.py
@@ -14,3 +14,14 @@ class AnObject:
     def __init__(self, value):
         self.value = value
 
+
+my_object = AnObject(14)
+random_keys[my_object] = "We can even store objects"
+my_object.value = 12
+try:
+    random_keys[[1, 2, 3]] = "We can't store lists though"
+except:
+    print("unable to store list\n")
+
+for key, value in random_keys.items():
+    print("{} has value {}".format(key, value))
