2178005497524c5d007e16169727fb8a47b9dabc
mohamadghoroobi <m.ghoroobi@gmail.com>
2023-06-03 09:44:58 +0330

    edit file dataclass_stocks.py
    add class StockRegClass, StockDecorated

--- Chapter06/dataclass_stocks.py
+++ Chapter06/dataclass_stocks.py
@@ -1,4 +1,23 @@
-from dataclasses import make_dataclass
+from dataclasses import make_dataclass, dataclass
 
-Stock = make_dataclass("Stock", "symbol", "cur", "high", "low")
-stock = Stock("FB", 177, 178, 175)
+Stock = make_dataclass("Stock", ["symbol", "cur", "high", "low"])
+stock = Stock("FB", 177, high=178, low=175)
+
+
+class StockRegClass:
+    def __init__(self, name, cur, high, low):
+        self.name = name
+        self.cur = cur
+        self.high = high
+        self.low = low
+
+
+stock_reg_class = StockRegClass("FB", 156, 45, 78)
+
+
+@dataclass
+class StockDecorated:
+    name: str
+    cur = float
+    high = float
+    low = float
