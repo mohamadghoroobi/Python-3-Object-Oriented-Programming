e2c9f81e4711ec9846e88f7304074c6f41fdeb40
mohamadghoroobi <m.ghoroobi@gmail.com>
2023-06-03 14:40:58 +0330

    edit file link_collector.py
    edit class LinkCollector
    
    update func init, collect_links

--- Chapter06/Case Study_ Link Collector/link_collector.py
+++ Chapter06/Case Study_ Link Collector/link_collector.py
@@ -10,22 +10,35 @@ LINK_REGEX = re.compile(
 class LinkCollector:
     def __init__(self, url):
         self.url = "" + urlparse(url).netloc
+        self.collected_links = set()
+        self.visited_links = set()
 
     def collect_links(self, path="/"):
         full_url = self.url + path
+        self.visited_links.add(full_url)
         page = str(urlopen(full_url).read())
         links = LINK_REGEX.findall(page)
-        print(links)
+        links = {self.normalize_url(path, link) for link in links}
+        self.collected_links = links.union(self.collected_links)
+        unvisited_links = links.difference(self.visited_links)
+        print(links, self.visited_links, self.collect_links, unvisited_links)
 
     def normalize_url(self, path, link):
-        if link.startwith("http://"):
-            return link
-        elif link.startwith("/"):
-            return self.url + link
+        if link.startswith("http://"):
+            return link.rstrip("/")
+        elif link.startswith("/"):
+            return self.url + link.rstrip("/")
         else:
-            return self.url + path.rpartition(
-                '/')[0] + '/' + link
+            return (
+                self.url
+                + path.rpartition("/")[0]
+                + "/"
+                + link.rstrip("/")
+            )
 
 
 if __name__ == "__main__":
-    LinkCollector(sys.argv[1]).collect_links()
+    collector = LinkCollector(sys.argv[1])
+    collector.collect_links()
+    for link in collector.collected_links:
+        print(link)
