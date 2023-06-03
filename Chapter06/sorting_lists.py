c0d1187d0e02a6435efba4f63b2e6a43ad009c23
mohamadghoroobi <m.ghoroobi@gmail.com>
2023-06-03 12:14:53 +0330

    edit file sorting_lists.py
    add class WeirdSortee
    
    add func init, lt, repr

---
+++ Chapter06/sorting_lists.py
@@ -0,0 +1,12 @@
+class WeirdSortee:
+    def __init__(self, string, number, sort_num):
+        self.string = string
+        self.number = number
+        self.sort_num = sort_num
+
+    def __lt__(self, object):
+        if self.sort_num:
+            return self.number < object.number
+
+    def __repr__(self):
+        return f"{self.string} : {self.number}"
