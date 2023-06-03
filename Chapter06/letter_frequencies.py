df3d16ff45c3bd8231e3cbbf52a3523236eca933
mohamadghoroobi <m.ghoroobi@gmail.com>
2023-06-03 11:47:49 +0330

    edit file letter_frequencies.py
    add func tuple_counter

--- Chapter06/letter_frequencies.py
+++ Chapter06/letter_frequencies.py
@@ -1,9 +1,22 @@
 from collections import defaultdict
 
 def letter_frequency(sentence):
-    frequencies = defaultdict
+    frequencies = defaultdict(float)
     for letter in sentence:
         # frequency = frequencies.setdefault(letter, 0)
         # frequencies[letter] = frequency + 1
         frequencies[letter] += 1
     return frequencies
+
+
+num_items = 0
+
+
+def tuple_counter():
+    global num_items
+    num_items += 1
+    return (num_items, [])
+
+
+d = defaultdict(tuple_counter)
+
