511008e1f478ed0af3817643412fb8d3ecc9cc67
mohamadghoroobi <m.ghoroobi@gmail.com>
2023-06-06 14:34:29 +0330

    edit file string_joiner.py
    add class StringJoiner

--- Chapter07/string_joiner.py
+++ Chapter07/string_joiner.py
@@ -1,7 +1,16 @@
+import random, string
+
+
 class StringJoiner(list):
     def __enter__(self):
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        self.result = "".join(self)
+        self.result = " ".join(self)
 
+
+with StringJoiner() as joiner:
+    for i in range(15):
+        joiner.append(random.choice(string.ascii_letters))
+
+print(joiner.result)
