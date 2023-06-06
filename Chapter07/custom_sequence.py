1c823433c4084defd105f0ba29800ecf212b1236
mohamadghoroobi <m.ghoroobi@gmail.com>
2023-06-06 14:03:16 +0330

    edit file custom_sequence.py
    add commands

--- Chapter07/custom_sequence.py
+++ Chapter07/custom_sequence.py
@@ -3,7 +3,7 @@ normal_list = [1, 2, 3, 4, 5]
 
 class CustomSequence:
     def __len__(self):
-        return 5
+        return 10
 
     def __getitem__(self, index):
         return f"x{index}"
@@ -12,3 +12,9 @@ class CustomSequence:
 class FunkyBackwards:
     def __reversed__(self):
         return "BACKWARDS"
+
+
+for seq in normal_list, CustomSequence(), FunkyBackwards():
+    print(f"\n{seq.__class__.__name__}: ", end="")
+    for item in reversed(seq):
+        print(item, end=", ")
