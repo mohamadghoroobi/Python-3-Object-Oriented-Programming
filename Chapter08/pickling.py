0d3b4ea5d5fa7b096a599f85cd017dc93bbef6ac
mohamadghoroobi <m.ghoroobi@gmail.com>
2023-06-10 09:46:07 +0330

    edit file pickling.py
    param some_data

--- Chapter08/pickling.py
+++ Chapter08/pickling.py
@@ -8,5 +8,11 @@ some_data = [
     [["inner"], ["list"]]
 ]
 
-with open ("pickled_list", "wb") as file:
+with open("pickled_list", "wb") as file:
     pickle.dump(some_data, file)
+
+with open("pickled_list", "rb") as file:
+    loaded_data = pickle.load(file)
+
+print(loaded_data)
+assert loaded_data == some_data
