32a9c6c035393aa4702c74d1062dd08bef0bc1bf
mohamadghoroobi <m.ghoroobi@gmail.com>
2023-06-10 13:50:48 +0330

    edit file warning_generators.py
    add class WarningFilter
    
    add result files

--- Chapter09/warning_generators.py
+++ Chapter09/warning_generators.py
@@ -1,9 +1,33 @@
-import sys
+# import sys
+#
+# inname, outname = sys.argv[1:3]
+#
+# with open(inname) as infile:
+#     with open(outname, "w") as outfile:
+#         for l in infile:
+#             if "WARNING" in l:
+#                 outfile.write(l.replace("WARNING", ""))
 
-inname, outname = sys.argv[1:3]
 
+class WarningFilter:
+    def __init__(self, insequence):
+        self.insequence = insequence
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        l = self.insequence.readline()
+        while l and "WARNING" not in l:
+            l = self.insequence.readline()
+        if not l:
+            raise StopIteration
+        return l.replace("WARNING", "")
+
+inname = "sample_log.txt"
+outname = "remove_warning_clas.txt"
 with open(inname) as infile:
     with open(outname, "w") as outfile:
-        for l in infile:
-            if "WARNING" in l:
-                outfile.write(l.replace("WARNING", ""))
+        filter = WarningFilter(infile)
+        for l in filter:
+            outfile.write(l)
