3891e23d457cadca5a900426412a041b83e9235e
mohamadghoroobi <m.ghoroobi@gmail.com>
2023-06-10 09:32:18 +0330

    edit file regex_check.py
    use check func

--- Chapter08/regex_check.py
+++ Chapter08/regex_check.py
@@ -1,7 +1,28 @@
 import re
+import sys
+
 
 search_string = "hello world"
 pattern = "hello world"
 match = re.match(pattern, search_string)
 if match:
     print("regex matches")
+
+
+# pattern = sys.argv[1]
+# search_string = sys.argv[2]
+# match = re.match(pattern, search_string)
+#
+# if match:
+#     template = "'{}' matches pattern '{}'"
+# else:
+#     template = "'{}' does not match pattern '{}'"
+# print(template.format(search_string, pattern))
+
+
+pattern = "^[a-zA-Z.]+@([a-z.]*\.[a-z]+)$"
+search_string = "some.user@example.com"
+match = re.match(pattern, search_string)
+if match:
+    domain = match.groups()[0]
+    print(domain)
