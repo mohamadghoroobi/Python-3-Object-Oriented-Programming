76782635afe520c98f35c6422c3bc3414e0614cf
mohamadghoroobi <m.ghoroobi@gmail.com>
2023-06-10 10:20:28 +0330

    add file json_serializer.py
    add class Contact
    
    add func full_name

---
+++ Chapter08/json_serializer.py
@@ -0,0 +1,8 @@
+class Contact:
+    def __init__(self, first, last):
+        self.first = first
+        self.last = last
+
+    @property
+    def full_name(self):
+        return ("{} {}".format(self.first, self.last))
