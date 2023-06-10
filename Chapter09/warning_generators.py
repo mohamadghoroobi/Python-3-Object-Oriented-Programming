69094c9649b30ae1c4165ddf138f41e8b8e0ce05
mohamadghoroobi <m.ghoroobi@gmail.com>
2023-06-10 14:04:15 +0330

    edit file warning_generators.py
    add func debug_filter

--- Chapter09/warning_generators.py
+++ Chapter09/warning_generators.py
@@ -31,3 +31,28 @@ with open(inname) as infile:
         filter = WarningFilter(infile)
         for l in filter:
             outfile.write(l)
+
+
+
+def warning_filter(insequence):
+    for l in insequence:
+        if "WARNING" in l:
+            yield l.replace("WARNING", "")
+
+with open(inname) as infile:
+    with open(outname, "w") as outfile:
+        filter = warning_filter(infile)
+        for l in filter:
+            outfile.write(l)
+
+inname = "sample_log.txt"
+outname = "remove_debug.txt"
+def debug_filter(infilename):
+    with open(infilename) as infile:
+        yield from (
+            l.replace("DEBUG", "") for l in infile if "DEBUG" in l
+        )
+filter = debug_filter(inname)
+with open(outname, "w") as outfile:
+    for l in filter:
+        outfile.write(l)
