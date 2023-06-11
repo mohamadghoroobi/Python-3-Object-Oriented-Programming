95c9fb545c72e8a61b502787a520eafa478b8e76
mohamadghoroobi <m.ghoroobi@gmail.com>
2023-06-11 12:41:00 +0330

    edit file main.py
    add class ConcreteDecoratorA

--- Chapter10/refactoring.guru/decorator/main.py
+++ Chapter10/refactoring.guru/decorator/main.py
@@ -44,4 +44,22 @@ class Decorator(Component):
         return self._component.operation()
 
 
+class ConcreteDecoratorA(Decorator):
+    """
+    Concrete Decorators call the wrapped object and alter its result in some
+    way.
+    """
+
+    def operation(self) -> str:
+        """
+        Decorators may call parent implementation of the operation, instead of
+        calling the wrapped object directly. This approach simplifies extension
+        of decorator classes.
+        """
+        return f"ConcreteDecoratorA({self.component.operation()})"
+
+
+
+
+
 
