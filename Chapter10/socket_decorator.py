fc7a5b1bbb1b0f5e8f9e41c727b3338e9d40954f
mohamadghoroobi <m.ghoroobi@gmail.com>
2023-06-11 14:32:33 +0330

    edit file socket_decorator.py
    add class LogSocket

--- Chapter10/socket_decorator.py
+++ Chapter10/socket_decorator.py
@@ -1,10 +1,23 @@
 import socket
 
 
+class LogSocket:
+    def __init__(self, socket):
+        self.socket = socket
+
+    def send(self, data):
+        print("Sending {0} to {1}".format(data, self.socket.getpeername()[0]))
+        self.socket.send(data)
+
+    def close(self):
+        self.socket.close()
+
+
 def respond(client):
     response = input("Enter a value: ")
     client.send(bytes(response, "utf8"))
     client.close()
+
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(("localhost", 2401))
