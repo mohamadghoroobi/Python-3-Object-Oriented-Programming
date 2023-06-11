662a37ae4fb632fa24f912563301fedaedab5d03
mohamadghoroobi <m.ghoroobi@gmail.com>
2023-06-11 12:41:20 +0330

    edit file main.py
    add class ConcreteDecoratorB

--- Chapter10/refactoring.guru/decorator/main.py
+++ Chapter10/refactoring.guru/decorator/main.py
@@ -59,6 +59,14 @@ class ConcreteDecoratorA(Decorator):
         return f"ConcreteDecoratorA({self.component.operation()})"
 
 
+class ConcreteDecoratorB(Decorator):
+    """
+    Decorators can execute their behavior either before or after the call to a
+    wrapped object.
+    """
+
+    def operation(self) -> str:
+        return f"ConcreteDecoratorB({self.component.operation()})"
 
 
 
