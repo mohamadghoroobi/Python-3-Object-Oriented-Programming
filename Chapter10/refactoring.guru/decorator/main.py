cd107c96ed79186e4b60cc592312374389b95956
mohamadghoroobi <m.ghoroobi@gmail.com>
2023-06-11 12:38:22 +0330

    edit file main.py
    add class ConcreteComponent

--- Chapter10/refactoring.guru/decorator/main.py
+++ Chapter10/refactoring.guru/decorator/main.py
@@ -6,3 +6,13 @@ class Component():
 
     def operation(self) -> str:
         pass
+
+
+class ConcreteComponent(Component):
+    """
+    Concrete Components provide default implementations of the operations. There
+    might be several variations of these classes.
+    """
+
+    def operation(self) -> str:
+        return "ConcreteComponent"
