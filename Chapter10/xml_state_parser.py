300caf4fef5346088a4cdc096149559b24a4d506
mohamadghoroobi <m.ghoroobi@gmail.com>
2023-06-17 11:14:00 +0330

    edit file xml_state_parser.py
    add func main

--- Chapter10/xml_state_parser.py
+++ Chapter10/xml_state_parser.py
@@ -70,19 +70,19 @@ class TextNode:
 
 class Parser:
     def __init__(self, parse_string):
-        self.pase_string = parse_string
+        self.parse_string = parse_string
         self.root = None
         self.current_node = None
 
         self.state = FirstTag()
 
-        def process(self, remaining_string):
-            remaining = self.state.process(remaining_string, self)
-            if remaining:
-                self.process(remaining)
+    def process(self, remaining_string):
+        remaining = self.state.process(remaining_string, self)
+        if remaining:
+            self.process(remaining)
 
-        def start(self):
-            self.process(self.parse_string)
+    def start(self):
+        self.process(self.parse_string)
 
 
 if __name__ == "__main__":
