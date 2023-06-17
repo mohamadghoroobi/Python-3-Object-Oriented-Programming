1d4335b275ed96270e35dae61372fad348936a73
mohamadghoroobi <m.ghoroobi@gmail.com>
2023-06-17 10:56:18 +0330

    edit file xml_state_parser.py
    add class OpenTag

--- Chapter10/xml_state_parser.py
+++ Chapter10/xml_state_parser.py
@@ -50,3 +50,15 @@ class ChildNode:
         else:
             parser.state = TextNode()
         return stripped
+
+
+class OpenTag:
+    def process(self, remaining_string, parser):
+        i_start_tag = remaining_string.find("<")
+        i_end_tag = remaining_string.find(">")
+        tag_name = remaining_string[i_start_tag + 1 : i_end_tag]
+        node = Node(tag_name, parser.current_node)
+        parser.current_node.children.append(node)
+        parser.current_node = node
+        parser.state = ChildNode()
+        return remaining_string[i_end_tag + 1 :]
