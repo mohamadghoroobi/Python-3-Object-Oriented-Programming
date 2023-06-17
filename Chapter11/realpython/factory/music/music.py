3a919e324c013640ca42d030a45a31ed92c7ece5
Mohammad Ghoroobi Lenove <m.ghoroobi@gmail.com>
2023-06-17 22:29:16 +0330

    edit file music.py
    add class SpotifyService

---
+++ Chapter11/realpython/factory/music/music.py
@@ -0,0 +1,6 @@
+class SpotifyService:
+    def __init__(self, access_code):
+        self._access_code = access_code
+
+    def test_connection(self):
+        print(f'Accessing Spotify with {self._access_code}')
