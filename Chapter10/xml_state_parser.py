e75431e22ac64016df3225b43defb95637e63484
mohamadghoroobi <m.ghoroobi@gmail.com>
2023-06-17 10:57:06 +0330

    edit file xml_state_parser.py
    add class TextNode

--- Chapter10/xml_state_parser.py
+++ Chapter10/xml_state_parser.py
@@ -69,8 +69,17 @@ class CloseTag:
         i_start_tag = remaining_string.find("<")
         i_end_tag = remaining_string.find(">")
         assert remaining_string[i_start_tag + 1] == "/"
-        tag_name = remaining_string[i_start_tag + 2 : i_end_tag]
+        tag_name = remaining_string[i_start_tag + 2: i_end_tag]
         assert tag_name == parser.current_node.tag_name
         parser.current_node = parser.current_node.parent
         parser.state = ChildNode()
-        return remaining_string[i_end_tag + 1 :].strip()
+        return remaining_string[i_end_tag + 1:].strip()
+
+
+class TextNode:
+    def process(self, remaining_string, parser):
+        i_start_tag = remaining_string.find("<")
+        text = remaining_string[:i_start_tag]
+        parser.current_node.text = text
+        parser.state = ChildNode()
+        return remaining_string[i_start_tag:]
