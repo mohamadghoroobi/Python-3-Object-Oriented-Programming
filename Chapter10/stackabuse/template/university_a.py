45d5bf4ba1a18d10302d00c014df4fdbfc16270c
mohamadghoroobi <m.ghoroobi@gmail.com>
2023-06-17 12:25:54 +0330

    add file university_a.py
    add class UniversityA

---
+++ Chapter10/stackabuse/template/university_a.py
@@ -0,0 +1,9 @@
+from research_guideline import ResearchGuideline
+
+
+class UniversityA(ResearchGuideline):
+    def step2(self):
+        print("Step 2 - Applied by University A")
+
+    def step3(self):
+        print("Step 3 - Applied by University A")
