feaf2ff36cfa6663579f4199e49e630f20765eda
mohamadghoroobi <m.ghoroobi@gmail.com>
2023-06-17 10:54:34 +0330

    add file xml_state_parser.py
    add class Parser

--- Chapter10/xml_state_parser.py
+++ Chapter10/xml_state_parser.py
@@ -10,3 +10,20 @@ class Node:
             return self.tag_name + ": " + self.text
         else:
             return self.tag_name
+
+
+class Parser:
+    def __init__(self, parse_string):
+        self.pase_string = parse_string
+        self.root = None
+        self.current_node = None
+
+        self.state = FirstTag()
+
+        def process(self, remaining_string):
+            remaining = self.state.process(remaining_string, self)
+            if remaining:
+                self.process(remaining)
+
+        def start(self):
+            self.process(self.parse_string)
