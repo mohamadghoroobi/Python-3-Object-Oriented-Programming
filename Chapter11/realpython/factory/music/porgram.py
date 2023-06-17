449b691de6d84d8c9b671942ca5b79c7ee27fbb2
Mohammad Ghoroobi Lenove <m.ghoroobi@gmail.com>
2023-06-17 22:28:29 +0330

    add file program.py

---
+++ Chapter11/realpython/factory/music/porgram.py
@@ -0,0 +1,7 @@
+config = {
+    'spotify_client_key': 'THE_SPOTIFY_CLIENT_KEY',
+    'spotify_client_secret': 'THE_SPOTIFY_CLIENT_SECRET',
+    'pandora_client_key': 'THE_PANDORA_CLIENT_KEY',
+    'pandora_client_secret': 'THE_PANDORA_CLIENT_SECRET',
+    'local_music_location': '/usr/data/music'
+}
