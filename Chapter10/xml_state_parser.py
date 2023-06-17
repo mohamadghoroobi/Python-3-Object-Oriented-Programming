308b8cff33961bfd8d3662d3ea167c0554fb0d53
mohamadghoroobi <m.ghoroobi@gmail.com>
2023-06-17 10:55:34 +0330

    add file xml_state_parser.py
    add class FirstTag

--- Chapter10/xml_state_parser.py
+++ Chapter10/xml_state_parser.py
@@ -27,3 +27,14 @@ class Parser:
 
         def start(self):
             self.process(self.parse_string)
+
+
+class FirstTag:
+    def process(self, remaining_string, parser):
+        i_start_tag = remaining_string.find("<")
+        i_end_tag = remaining_string.find(">")
+        tag_name = remaining_string[i_start_tag + 1 : i_end_tag]
+        root = Node(tag_name)
+        parser.root = parser.current_node = root
+        parser.state = ChildNode()
+        return remaining_string[i_end_tag + 1 :]
