604db79ff76a9881880c35a191e5c4ab144733c9
Mohammad Ghoroobi Lenove <m.ghoroobi@gmail.com>
2023-06-17 22:53:39 +0330

    edit file serializers.py
    add class SerializerFactory

--- Chapter11/realpython/factory/Serializer/serializers.py
+++ Chapter11/realpython/factory/Serializer/serializers.py
@@ -33,8 +33,41 @@ class XmlSerializer:
         return et.tostring(self._element, encoding='unicode')
 
 
+class SerializerFactory:
+    def get_serializer(self, format):
+        if format == 'JSON':
+            return JsonSerializer()
+        elif format == 'XML':
+            return XmlSerializer()
+        else:
+            raise ValueError(format)
+
+
+# factory = SerializerFactory()
+
+
 class ObjectSerializer:
     def serialize(self, serializable, format):
         serializer = factory.get_serializer(format)
         serializable.serialize(serializer)
         return serializer.to_str()
+
+
+class SerializerFactory:
+
+    def __init__(self):
+        self._creators = {}
+
+    def register_format(self, format, creator):
+        self._creators[format] = creator
+
+    def get_serializer(self, format):
+        creator = self._creators.get(format)
+        if not creator:
+            raise ValueError(format)
+        return creator()
+
+
+factory = SerializerFactory()
+factory.register_format('JSON', JsonSerializer)
+factory.register_format('XML', XmlSerializer)
