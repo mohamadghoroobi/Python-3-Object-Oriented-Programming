fcb3fa1ad6d05e7d3e33f254b35caddf96b217ab
mohamadghoroobi <m.ghoroobi@gmail.com>
2023-06-17 10:56:44 +0330

    edit file xml_state_parser.py
    add class CloseTag

--- Chapter10/xml_state_parser.py
+++ Chapter10/xml_state_parser.py
@@ -56,9 +56,21 @@ class OpenTag:
     def process(self, remaining_string, parser):
         i_start_tag = remaining_string.find("<")
         i_end_tag = remaining_string.find(">")
-        tag_name = remaining_string[i_start_tag + 1 : i_end_tag]
+        tag_name = remaining_string[i_start_tag + 1: i_end_tag]
         node = Node(tag_name, parser.current_node)
         parser.current_node.children.append(node)
         parser.current_node = node
         parser.state = ChildNode()
-        return remaining_string[i_end_tag + 1 :]
+        return remaining_string[i_end_tag + 1:]
+
+
+class CloseTag:
+    def process(self, remaining_string, parser):
+        i_start_tag = remaining_string.find("<")
+        i_end_tag = remaining_string.find(">")
+        assert remaining_string[i_start_tag + 1] == "/"
+        tag_name = remaining_string[i_start_tag + 2 : i_end_tag]
+        assert tag_name == parser.current_node.tag_name
+        parser.current_node = parser.current_node.parent
+        parser.state = ChildNode()
+        return remaining_string[i_end_tag + 1 :].strip()
