a91f59cc04ce06d6d8e3499246911b66597d3902
Mohammad Ghoroobi Lenove <m.ghoroobi@gmail.com>
2023-06-17 22:29:45 +0330

    edit file music.py
    add class SpotifyServiceBuilder

--- Chapter11/realpython/factory/music/music.py
+++ Chapter11/realpython/factory/music/music.py
@@ -4,3 +4,18 @@ class SpotifyService:
 
     def test_connection(self):
         print(f'Accessing Spotify with {self._access_code}')
+
+
+class SpotifyServiceBuilder:
+    def __init__(self):
+        self._instance = None
+
+    def __call__(self, spotify_client_key, spotify_client_secret, **_ignored):
+        if not self._instance:
+            access_code = self.authorize(
+                spotify_client_key, spotify_client_secret)
+            self._instance = SpotifyService(access_code)
+        return self._instance
+
+    def authorize(self, key, secret):
+        return 'SPOTIFY_ACCESS_CODE'
