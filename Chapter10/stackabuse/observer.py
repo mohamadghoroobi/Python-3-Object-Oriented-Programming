c52a2b6afc91a42b5ed7a63bc3ff901c91ad6f99
mohamadghoroobi <m.ghoroobi@gmail.com>
2023-06-17 09:07:56 +0330

    edit file observer.py
    add class Observable

--- Chapter10/stackabuse/observer.py
+++ Chapter10/stackabuse/observer.py
@@ -5,3 +5,17 @@ class Observer:
     def notify(self, observable, *args, **kwargs):
         print("Got", args, kwargs, "From", observable)
 
+
+class Observable:
+    def __init__(self):
+        self._observers = []
+
+    def subscribe(self, observer):
+        self._observers.append(observer)
+
+    def notify_observers(self, *args, **kwargs):
+        for obs in self._observers:
+            obs.notify(self, *args, **kwargs)
+
+    def unsubscribe(self, observer):
+        self._observers.remove(observer)
